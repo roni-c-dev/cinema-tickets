e6250c8fce7ecd5605fe6b193c33e6aba64a43e9
roni-c-dev <107492068+roni-c-dev@users.noreply.github.com>
2024-10-22 19:03:53 +0100

    refactor(all): restructure some tests, add JSDocs, update scripts

--- src/pairtest/lib/InvalidPurchaseException.js
+++ src/pairtest/lib/InvalidPurchaseException.js
@@ -1,5 +1,6 @@
 export default class InvalidPurchaseException extends Error {
     constructor(message) {
         super(message);
-    }
+        this.name = "InvalidPurchaseException"; // Set the name for the error
+    }   
 }
